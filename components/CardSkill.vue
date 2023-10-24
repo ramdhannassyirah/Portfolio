5497fc5d31e8eef690470b78c8a1b49619e7a57d
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2023-10-24 15:45:03 +0700

    Portfolio

--- components/CardSkill.vue
+++ components/CardSkill.vue
@@ -1,47 +1,55 @@
 <template>
   <div
-    class="sm:bg-[#F4EEE6] sm:mx-auto sm:w-50% sm:mx-10 rounded-[80px] sm:px-10 sm:grid sm:grid-cols-2 justify-items-center"
+    class="sm:bg-[#f9f7f5] sm:mx-auto sm:w-50% sm:mx-10 rounded-[80px] sm:px-10 sm:grid sm:grid-cols-2 justify-items-center"
   >
-    <div class="m-10">
+    <div
+      class="bg-[#f9f7f5] rounded-xl sm:rounded-none sm:bg-transparent p-5 sm:p-0 m-10"
+    >
       <h1 class="font-playpen text-[#025A4E] font-bold">01</h1>
       <h1 class="font-playpen text-[#025A4E] font-bold my-2 text-xl">
         Make it
       </h1>
-      <h1 class="font-roboto">
+      <h1 class="font-sans text-gray-500">
         I sketch wireframes and make prototypes. Talking through tactile designs
         existing in the browser is worth its weight. Design tools only carry you
         so far; the rest should be realized with a link my team can rally
         around.
       </h1>
     </div>
-    <div class="m-10">
+    <div
+      class="bg-[#f9f7f5] rounded-xl sm:rounded-none sm:bg-transparent p-5 sm:p-0 m-10"
+    >
       <h1 class="font-playpen text-[#025A4E] font-bold">02</h1>
       <h1 class="font-playpen text-[#025A4E] font-bold my-2 text-xl">
         Make it
       </h1>
-      <h1 class="font-roboto">
+      <h1 class="font-sans text-gray-500">
         I sketch wireframes and make prototypes. Talking through tactile designs
         existing in the browser is worth its weight. Design tools only carry you
       </h1>
     </div>
-    <div class="m-10">
+    <div
+      class="bg-[#f9f7f5] rounded-xl sm:rounded-none sm:bg-transparent p-5 sm:p-0 m-10"
+    >
       <h1 class="font-playpen text-[#025A4E] font-bold">03</h1>
       <h1 class="font-playpen text-[#025A4E] font-bold my-2 text-xl">
         Make it
       </h1>
-      <h1 class="font-roboto">
+      <h1 class="font-sans text-gray-500">
         I sketch wireframes and make prototypes. Talking through tactile designs
         existing in the browser is worth its weight. Design tools only carry you
         so far; the rest should be realized with a link my team can rally
         around.
       </h1>
     </div>
-    <div class="m-10">
+    <div
+      class="bg-[#f9f7f5] rounded-xl sm:rounded-none sm:bg-transparent p-5 sm:p-0 m-10"
+    >
       <h1 class="font-playpen text-[#025A4E] font-bold">04</h1>
       <h1 class="font-playpen text-[#025A4E] font-bold my-2 text-xl">
         Make it
       </h1>
-      <h1 class="font-roboto">
+      <h1 class="font-sans text-gray-500">
         I sketch wireframes and make prototypes. Talking through tactile designs
         existing in the browser is worth its weight. Design tools only carry you
         so far; the rest should be realized with a link my team can rally
