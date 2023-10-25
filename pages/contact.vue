33d9d2b21065b5fd2492a6056ed28d1fe784af9f
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2023-10-25 19:16:12 +0700

    portofolio

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center min-h-screen">
     <div class="p-4 bg-white rounded-lg">
       <!-- Konten div yang ingin Anda tengahkan -->
-      <h1 class="text-xl font-semibold">AKAN DATANG</h1>
+      <h1 class="text-xl font-semibold">SOON</h1>
     </div>
   </div>
 </template>
--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -12,57 +12,75 @@
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:html5" />
       <h1 class="ml-1">HTML</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:css3-simple" />
       <h1 class="ml-1">CSS DASAR</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:css3-simple" />
       <h1 class="ml-1">CSS Layouting</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:css3-simple" />
       <h1 class="ml-1">CSS 3</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <h1 class="ml-1">Bootstrap</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:css3-simple" />
       <h1 class="ml-1">Flexbox CSS</h1>
     </div>
 
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <Icon name="uil:github" />
       <h1 class="ml-1">Git & Github</h1>
     </div>
-
     <div
-      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
     >
       <h1 class="ml-1">Tailwind CSS</h1>
+    </div>
+
+    <div
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:java-script" />
+      <h1 class="ml-1">Javascipt Dasar</h1>
+    </div>
+    <div
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:java-script" />
+      <h1 class="ml-1">Javascript DOM</h1>
+    </div>
+    <div
+      class="font-semibold sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:java-script" />
+      <h1 class="ml-1">Javascript Lanjutan</h1>
     </div>
   </div>
 </template>
