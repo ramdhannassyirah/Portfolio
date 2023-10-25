424f04bd770349fae0055e8539b806fdc9c504c4
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2023-10-25 18:09:00 +0700

    portofolio

--- components/Card/CardH.vue
+++ components/Card/CardH.vue
@@ -2,12 +2,24 @@
   <div
     class="container w-96 h-96 rounded-3xl sm:h-[400px] sm:w-46 overflow-hidden card"
   >
-    <div class="p-10 text-center sm:text-right">
-      <h4 class="font-medium font-roboto">Project</h4>
-      <h1 class="text-xl font-semibold font-playpen">Article</h1>
+    <div
+      class="flex flex-col-reverse items-center py-10 text-center sm:flex-row sm:justify-between"
+    >
+      <div
+        class="flex items-center justify-center my-2 text-center cursor-pointer rounded-xl"
+      >
+        <Icon name="uil:link" />
+        <a class="mx-1 text-lg font-semibold hover:underline" :href="href"
+          >Link</a
+        >
+      </div>
+      <div class="">
+        <h4 class="font-medium font-roboto">Project</h4>
+        <h1 class="text-xl font-semibold font-playpen">{{ name }}</h1>
+      </div>
     </div>
     <div class="zoom-on-hover">
-      <img class="rounded-xl" src="http://source.unsplash.com/random" alt="" />
+      <a :href="href"> <img class="rounded-xl" :src="image" alt="" /></a>
     </div>
   </div>
 </template>
@@ -25,6 +37,15 @@
 
 .card:hover {
   transform: scale(1.1); /* Scaling saat hover sebesar 110% */
-  box-shadow: 0px 0px 55px 2px rgba(0, 0, 0, 0.5); /* Bayangan saat hover */
+  box-shadow: 0px 0px 50px 2px rgba(0, 0, 0, 0.4); /* Bayangan saat hover */
 }
 </style>
+<script>
+export default {
+  props: {
+    image: null,
+    name: String,
+    href: String,
+  },
+};
+</script>
