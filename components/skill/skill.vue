446288ec2f3ab33e45713a59438dfb98d2b6f6ae
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2023-10-25 18:28:10 +0700

    portofolio

--- components/skill/skill.vue
+++ components/skill/skill.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="bg-[#ffffff] w-[150px] h-[40px] box-border rounded-full flex justify-center items-center"
+    class="bg-[#025A4E] w-[150px] h-[40px] box-border rounded-full flex justify-center items-center"
   >
-    <p class="font-semibold text-[13px] sm:text-xl text-[#025A4E]">
+    <p class="font-semibold text-[13px] sm:text-xl text-[#ffffff]">
       {{ name }}
     </p>
   </div>
