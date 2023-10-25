0ce52ceb51b6593d822290108356018ec2b0b813
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2023-10-25 19:05:30 +0700

    portofolio

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -1,8 +1,68 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="p-4 bg-white rounded-lg">
-      <!-- Konten div yang ingin Anda tengahkan -->
-      <h1 class="text-xl font-semibold">AKAN DATANG</h1>
+  <div
+    class="grid items-center min-h-screen grid-flow-row justify-items-center"
+  >
+    <div class="my-5">
+      <h1 class="text-3xl font-bold text-center font-playpen text-[#025A4E]">
+        Roadmap
+      </h1>
+      <p class="mt-1 text-sm">
+        Learning path recomendation and free course playlist
+      </p>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:html5" />
+      <h1 class="ml-1">HTML</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:css3-simple" />
+      <h1 class="ml-1">CSS DASAR</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:css3-simple" />
+      <h1 class="ml-1">CSS Layouting</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:css3-simple" />
+      <h1 class="ml-1">CSS 3</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <h1 class="ml-1">Bootstrap</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:css3-simple" />
+      <h1 class="ml-1">Flexbox CSS</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <Icon name="uil:github" />
+      <h1 class="ml-1">Git & Github</h1>
+    </div>
+
+    <div
+      class="sm:w-[50%] w-[80%] bg-white rounded-xl px-5 h-[40px] flex items-center my-1 hover:drop-shadow-xl transition-transform hover:scale-105"
+    >
+      <h1 class="ml-1">Tailwind CSS</h1>
     </div>
   </div>
 </template>
